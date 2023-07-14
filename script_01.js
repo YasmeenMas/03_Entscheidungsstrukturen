179ada1ccf4d9de795a995fe4b0e3b50fd136de3
Yasmeen <yasmeenmas@gmx.de>
2023-07-14 11:40:11 +0200

    IF - ELSE IF

--- script_01.js
+++ script_01.js
@@ -27,9 +27,46 @@ console.log("------------------");
 
 //if (false) // fake it, till you make it!
 // if (isJohnOlder) 
-if (ageJohn > ageMark) 
+// if (ageJohn > ageMark) 
+// {
+//     console.log("John ist älter.");
+// }
+
+/************ IF - ELSE ************/
+// mit Alternative
+// entweder ja oder nein
+
+// if (true)
+// if (false)
+// if (isJohnOlder)
+// if ( ageJohn > ageMark)
+// {
+//     console.log("John ist älter.");
+// } 
+// else 
+// {
+//     console.log("John ist jünger.");
+// }
+
+/************ Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+/************ IF - ELSE IF ************/
+// mit alternativen Fällen (älter, gleich alt, jünger)
+
+//1. Test
+if (isJohnOlder)
 {
     console.log("John ist älter.");
+} 
+// 1.....n Alternative 
+else if(isJohnEqual)
+{
+    console.log("John ist gleich alt.");
 }
-
-
+// wenn alle vorherigen Tests fehlschlagen
+else 
+{
+    console.log("John ist jünger.");
+}
