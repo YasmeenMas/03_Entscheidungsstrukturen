9785cc5e90c2667e882b3fe13f58dfcdd2e6e306
Yasmeen <yasmeenmas@gmx.de>
2023-07-14 12:46:12 +0200

    Fallunterscheidung / SWITCH|CASE 2

--- script_01.js
+++ script_01.js
@@ -74,36 +74,55 @@
 
 /****** Fallunterscheidung / SWITCH|CASE 1 ******/
 
-const firstName = "Jane";
-let job;
+// const firstName = "Jane";
+// let job;
 
 // job = prompt("Welchen Job hast du?")
 
-job = "driver";         // .. fährt TAXI! / UBER
-job = "diver";          // .. taucht im Rhein!
-job = "artist";         // .. malt ein Bild!
-job = "pilot";          // .. macht etwas anderes! --> default
-job = "teacher";        // .. unterrichtet!
-job = "instructor";     // .. unterrichtet!
+// job = "driver";         // .. fährt TAXI! / UBER
+// job = "diver";          // .. taucht im Rhein!
+// job = "artist";         // .. malt ein Bild!
+// job = "pilot";          // .. macht etwas anderes! --> default
+// job = "teacher";        // .. unterrichtet!
+// job = "instructor";     // .. unterrichtet!
 
-switch (job) {
-    case "driver":
-        console.log("Jane fährt TAXI!");
-        break;
-    case "diver":
-        console.log("Jane taucht im Rhein!");
-        break;
-    case "artist":
-        console.log("Jane malt ein Bild!");
-        break;
-    // case "teacher":
-    // case "instructor":
-    case "instructor" || "teacher":
-        console.log("Jane unterrichtet!");
-        break;
+// switch (job) {
+//     case "driver":
+//         console.log("Jane fährt TAXI!");
+//         break;
+//     case "diver":
+//         console.log("Jane taucht im Rhein!");
+//         break;
+//     case "artist":
+//         console.log("Jane malt ein Bild!");
+//         break;
+//     // case "teacher":
+//     // case "instructor":
+//     case "instructor" || "teacher":
+//         console.log("Jane unterrichtet!");
+//         break;
     
-    default: // default zuerst!
-        console.log("Jane macht etwas anderes!");
-        break;           
-}
+//     default: // default zuerst!
+//         console.log("Jane macht etwas anderes!");
+//         break;           
+// }
 
+/******** Fallunterscheidung / SWITCH|CASE 2 *******/
+
+const cond = true;
+const a = 2;
+
+switch (cond) {
+    case (a==1):
+        console.log("a ist gleich 1!");
+        break;
+
+    case (a==2):
+        console.log("a ist gleich 2!");
+        break;
+
+    default:
+        console.log("a hat einen anderen Wert!");
+        break;
+
+}
